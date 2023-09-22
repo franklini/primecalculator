add67d317d8e5d3412449976b1e266a36b5e0b27
franklinike <franklin@digitalfineprint.com>
2023-09-22 09:47:53 +0100

    Make changes to focus of good programming principles rather than catering the solution for analysis and individual cache of each algorithm results

--- src/main/java/com/natwest/primecalculator/enums/SieveEnum.java
+++ src/main/java/com/natwest/primecalculator/enums/SieveEnum.java
@@ -1,31 +1,13 @@
 package com.natwest.primecalculator.enums;
 
-import com.natwest.primecalculator.service.SieveService;
-import com.natwest.primecalculator.service.impl.SieveOfEratosthenesV1ServiceImpl;
-import com.natwest.primecalculator.service.impl.SieveOfEratosthenesV2ServiceImpl;
-import com.natwest.primecalculator.service.impl.SieveOfEratosthenesV3ServiceImpl;
-import com.natwest.primecalculator.service.impl.SieveOfEratosthenesV4ServiceImpl;
-import com.natwest.primecalculator.service.impl.SieveOfAtkinsServiceImpl;
-import com.natwest.primecalculator.service.impl.SieveOfSundaramServiceImpl;
-
 /**
  * Enum for managing the rest endpoints for the different sieves.
  */
 public enum SieveEnum {
-    ERATOSTHENESV1(new SieveOfEratosthenesV1ServiceImpl()),
-    ERATOSTHENESV2(new SieveOfEratosthenesV2ServiceImpl()),
-    ERATOSTHENESV3(new SieveOfEratosthenesV3ServiceImpl()),
-    ERATOSTHENESV4(new SieveOfEratosthenesV4ServiceImpl()),
-    ATKIN(new SieveOfAtkinsServiceImpl()),
-    SUNDARAM(new SieveOfSundaramServiceImpl());
-
-    private SieveService sieveService;
-
-    SieveEnum(final SieveService sieveService){
-        this.sieveService = sieveService;
-    }
-
-    public SieveService getSieveInterface() {
-        return sieveService;
-    }
+    ERATOSTHENESV1,
+    ERATOSTHENESV2,
+    ERATOSTHENESV3,
+    ERATOSTHENESV4,
+    ATKIN,
+    SUNDARAM;
 }
